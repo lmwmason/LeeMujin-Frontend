1db73c679bbbdb4ef6abf00b3ad2257c2794a853
happy coding <lmwmason@naver.com>
2025-09-20 20:33:27 +0900

    Frontend Done!!

--- leemujin_site/src/components/Footer.js
+++ leemujin_site/src/components/Footer.js
@@ -14,6 +14,8 @@ const Footer = () => {
                     <ul>
                         <li><a href="https://www.youtube.com/@leemujin" target="_blank" rel="noopener noreferrer">이무진 공식 유튜브</a></li>
                         <li><a href="https://www.instagram.com/morilla_mujin/" target="_blank" rel="noopener noreferrer">이무진 인스타그램</a></li>
+                        <li><a href="https://m.cafe.daum.net/LEEMUJIN/_rec" target="_blank" rel="noopener noreferrer">이무진 공식 팬카페</a></li>
+                        <li><a href="https://cafe.naver.com/loveleemujin" target="_blank" rel="noopener noreferrer">이무진 네이버 팬카페</a></li>
                         <li><a href="https://namu.wiki/w/%EC%9D%B4%EB%AC%B4%EC%A7%84" target="_blank" rel="noopener noreferrer">이무진 나무위키</a></li>
                         <li><a href="https://ko.wikipedia.org/wiki/%EC%9D%B4%EB%AC%B4%EC%A7%84" target="_blank" rel="noopener noreferrer">이무진 위키백과</a></li>
                     </ul>
